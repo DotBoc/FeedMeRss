543aef38fc3aed10eb5637c8f277f95fb760bef1
BotDoc <trfdoc@gmail.com>
2018-01-30 21:25:12 +0200

    Toolbar added never touch it i got no idea how it worked

--- app/src/main/java/com/feedmerss/lidlproduction/feedmerss/Main.java
+++ app/src/main/java/com/feedmerss/lidlproduction/feedmerss/Main.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class Main extends AppCompatActivity {
 
@@ -16,5 +19,17 @@ public class Main extends AppCompatActivity {
         setSupportActionBar(toolbar);
         Intent intent = getIntent();
         final String url = intent.getStringExtra("FIREBASE_URL");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
