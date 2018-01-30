aed0f95723d5ec8e6bbdb55436b985fff8339581
BotDoc <trfdoc@gmail.com>
2018-01-30 21:03:32 +0200

    Toolbar added never touch it i got no idea how it worked

--- app/src/main/java/com/feedmerss/lidlproduction/feedmerss/Login.java
+++ app/src/main/java/com/feedmerss/lidlproduction/feedmerss/Login.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -33,6 +34,9 @@ public class Login extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.custom_actionbar);
+        setSupportActionBar(toolbar);
 
         mEmailField = findViewById(R.id.ETEmail);
         mPasswordField = findViewById(R.id.ETPassword);
--- app/src/main/java/com/feedmerss/lidlproduction/feedmerss/Main.java
+++ app/src/main/java/com/feedmerss/lidlproduction/feedmerss/Main.java
@@ -3,6 +3,7 @@ package com.feedmerss.lidlproduction.feedmerss;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class Main extends AppCompatActivity {
 
@@ -11,6 +12,8 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar)findViewById(R.id.custom_actionbar_with_components);
+        setSupportActionBar(toolbar);
         Intent intent = getIntent();
         final String url = intent.getStringExtra("FIREBASE_URL");
     }
