2d08c9a21e3b8deace6740bb44af5b5b4fbb99c7
natasa <ber3danna@gmail.com>
2018-02-04 17:21:26 +0200

    evala id kai names se ola kai sumplhrwsa kai to eggafo sto drive me ta rss

--- app/src/main/java/com/feedmerss/lidlproduction/feedmerss/Settings_Add_RSS.java
+++ app/src/main/java/com/feedmerss/lidlproduction/feedmerss/Settings_Add_RSS.java
@@ -4,13 +4,26 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Settings_Add_RSS extends AppCompatActivity {
+
+    private TextView AddFeed;
+    private Button Save;
+    private EditText RssName;
+    private EditText RssLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings__add__rss);
+
+         AddFeed = findViewById(R.id.TVRssFeed);
+         Save = findViewById(R.id.BSave);
+         RssName = findViewById(R.id.ETRssName);
+         RssLink = findViewById(R.id.ETRssLink);
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.custom_actionbar);
         setSupportActionBar(toolbar);
--- app/src/main/java/com/feedmerss/lidlproduction/feedmerss/Settings_Basic_RSS_selection.java
+++ app/src/main/java/com/feedmerss/lidlproduction/feedmerss/Settings_Basic_RSS_selection.java
@@ -4,13 +4,33 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.CheckBox;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Settings_Basic_RSS_selection extends AppCompatActivity {
+
+    private TextView BasicRss;
+    private CheckBox Health,Sports,Films,Finance,Politics,Weather,Lifestyle,Theatre,Travel,Animals;
+    private Button Save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings__basic__rss_selection);
+
+        BasicRss = findViewById(R.id.TVBasicRss);
+        Health = findViewById(R.id.CBHealth);
+        Sports = findViewById(R.id.CBSports);
+        Films = findViewById(R.id.CBFilms);
+        Finance = findViewById(R.id.CBFinance);
+        Politics = findViewById(R.id.CBPolitics);
+        Weather = findViewById(R.id.CBWeather);
+        Lifestyle = findViewById(R.id.CBLifestyle);
+        Theatre = findViewById(R.id.CBTheatre);
+        Travel = findViewById(R.id.CBTravel);
+        Animals = findViewById(R.id.CBAnimals);
+        Save = findViewById(R.id.BSave);
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.custom_actionbar);
         setSupportActionBar(toolbar);
