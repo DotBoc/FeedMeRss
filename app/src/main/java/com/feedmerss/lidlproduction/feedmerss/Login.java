12384af6290a65233f1bf78ccd9721135b1d005c
BotDoc <trfdoc@gmail.com>
2018-02-06 19:06:35 +0200

    Drawer buttons complete

--- app/src/main/java/com/feedmerss/lidlproduction/feedmerss/Login.java
+++ app/src/main/java/com/feedmerss/lidlproduction/feedmerss/Login.java
@@ -70,7 +70,7 @@ public class Login extends AppCompatActivity implements
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            gotoActivity(Main.class);
+                            gotoActivity(Settings_Basic_RSS_selection.class);
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "createUserWithEmail:failure", task.getException());
